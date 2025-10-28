38d0b7d3197e24c449221b7604e640486882bef4
jafari <@jafari>
2025-10-28 15:46:37 +0330

    add migrations and Repository

---
+++ Data/Repository.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Minilibrary.Interfaces;
+
+namespace Minilibrary.Data
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly AppDbContext _context;
+        private readonly DbSet<T> _dbSet;
+        public Repository(AppDbContext context)
+        {
+            _context = context;
+            _dbSet = _context.Set<T>();
+        }
+
+        public async Task<T?> GetByIdAsync(int id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<T>?> GetAllAsync()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        public async Task<T> CreateAsync(T entity)
+        {
+            await _dbSet.AddAsync(entity);
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task<ICollection<T>> BulkCreateAsync(IEnumerable<T> entities)
+        {
+            await _dbSet.AddRangeAsync(entities);
+            await _context.SaveChangesAsync();
+            return [.. entities];
+        }
+
+        public async Task<T> UpdateAsync(T entity)
+        {
+            _dbSet.Update(entity);
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task<bool> DeleteAsync(int id)
+        {
+            var existsEntity = await GetByIdAsync(id);
+
+            if (existsEntity == null)
+                return false;
+
+            _dbSet.Remove(existsEntity);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+    }
+}
---
+++ Interfaces/IRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace Minilibrary.Interfaces
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<T?> GetByIdAsync(int id);
+        Task<IEnumerable<T>?> GetAllAsync();
+        Task<T> CreateAsync(T entity);
+        Task<ICollection<T>> BulkCreateAsync(IEnumerable<T> entities);
+        Task<T> UpdateAsync(T entity);
+        Task<bool> DeleteAsync(int id);
+    }
+}
