13a72411c641f11bbfe6f32e93072c8c1b7ceee4
jafari <@jafari>
2025-10-29 09:47:04 +0330

    add string MaxLength

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,9 +1,23 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Minilibrary.Entites;
 
 namespace Minilibrary.Data
 {
     public class AppDbContext : DbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Book> Books { get; set; }
+        public DbSet<Subject> Subjects { get; set; }
+        public DbSet<Location> Locations { get; set; }
+
+        protected override void ConfigureConventions(ModelConfigurationBuilder configurationBuilder)
+        {
+            base.ConfigureConventions(configurationBuilder);
+
+            configurationBuilder
+                .Properties<string>()
+                .HaveMaxLength(300);
+        }
     }
 }
--- Migrations/20251029061639_add string MaxLength.Designer.cs
+++ Migrations/20251029061639_add string MaxLength.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Minilibrary.Data;
 
@@ -10,9 +11,11 @@ using Minilibrary.Data;
 namespace Minilibrary.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251029061639_add string MaxLength")]
+    partial class addstringMaxLength
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
