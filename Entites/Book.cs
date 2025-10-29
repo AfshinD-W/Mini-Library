a03667ce23f720b8f13aef07f80ac468e7fb08bd
jafari <afshinaa705@gmail.com>
2025-10-29 09:04:57 +0330

    Create entities

---
+++ Entites/Book.cs
@@ -0,0 +1,14 @@
+﻿namespace Minilibrary.Entites
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        public ICollection<Subject> Subjects { get; set; } = new List<Subject>();
+
+        public int LocationId { get; set; }
+        public Location Location { get; set; }
+    }
+}
