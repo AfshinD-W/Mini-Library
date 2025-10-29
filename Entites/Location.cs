468f8e52e074f005e1657dd92a09768d0bfd9389
jafari <@jafari>
2025-10-29 09:04:57 +0330

    Create entities

---
+++ Entites/Location.cs
@@ -0,0 +1,11 @@
+﻿namespace Minilibrary.Entites
+{
+    public class Location
+    {
+        public int Id { get; set; }
+        public string Row { get; set; }
+        public string RowNumber { get; set; }
+
+        public ICollection<Book> Books { get; set; } = new List<Book>();
+    }
+}
---
+++ Entites/Subject.cs
@@ -0,0 +1,10 @@
+﻿namespace Minilibrary.Entites
+{
+    public class Subject
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public ICollection<Book> books { get; set; } = new List<Book>();
+    }
+}
